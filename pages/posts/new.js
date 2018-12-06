d61505cab7a0da0612d58710d948a034a428d319
Chiwei Huang <cwhuang0523@gmail.com>
2018-12-07 00:19:40 +0800

    complete post belongs to city when create

--- pages/posts/new.js
+++ pages/posts/new.js
@@ -22,7 +22,8 @@ Page({
         post: {
           // name: e.detail.value.name,
           name: page.data.name,
-          description: e.detail.value.description
+          description: e.detail.value.description,
+          address: page.data.address
         }
       },
       success(res) {
@@ -43,44 +44,6 @@ Page({
       }
     })
   },
-
-  grantAuthorizeLocation: function () {
-    var that = this;
-    app.getPermission(that); 
-    wx.getSetting({
-      success(res) {
-        console.log(22, res)
-      }
-    })
-    wx.getSetting({
-      success(res) {
-        console.log(22, res)
-      }
-    })
-    let page = this
-    wx.authorize({
-      scope: 'scope.userLocation',
-      success(res) {
-        console.log(33, res)
-        wx.chooseLocation({
-          success: function (res) {
-            console.log(44, res)
-            page.setData({ 
-              name: res.name,
-              address: res.address,
-              latitude: res.latitude,
-              longitude: res.longitude
-             })
-            // console.log(page.data)
-          },
-        })
-      },
-      fail(err) {
-        console.log(44, err)
-      }
-    })
-  },
-
   getPlace: function () {
     var page = this;
     app.getPermission(page); // Enter     that value to set the content directly on the app.js page  
