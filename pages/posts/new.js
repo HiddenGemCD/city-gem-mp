cffa078bec09d925aecc8f2894d25b9935395c61
Chiwei Huang <cwhuang0523@gmail.com>
2018-12-07 11:26:51 +0800

    complete select category

--- pages/posts/new.js
+++ pages/posts/new.js
@@ -3,8 +3,21 @@ const myRequest = require('../../lib/api/request');
 
 Page({
   data: {
-    name: 'Choose Location'
-    
+    name: 'Choose Location',
+    tabTxt: [
+      {
+        'text': 'Category',
+        'originalText': 'Category',
+        'active': false,
+        'child': [
+          { 'id': 1, 'text': 'eat' },
+          { 'id': 2, 'text': 'drink' },
+          { 'id': 3, 'text': 'play' }
+        ],
+        'type': 0
+      }
+    ],
+    searchParam: []
   },
   // Form Submit Button
   onLoad: function () {
@@ -23,7 +36,8 @@ Page({
           // name: e.detail.value.name,
           name: page.data.name,
           description: e.detail.value.description,
-          address: page.data.address
+          address: page.data.address,
+          category: page.data.searchParam[0]
         }
       },
       success(res) {
@@ -47,5 +61,54 @@ Page({
   getPlace: function () {
     var page = this;
     app.getPermission(page); // Enter     that value to set the content directly on the app.js page  
+  },
+
+  // drop down js
+  filterTab: function (e) {
+    var that = this;
+    var data = JSON.parse(JSON.stringify(that.data.tabTxt));
+    var index = e.currentTarget.dataset.index;
+    var newTabTxt = data.map(function (e) {
+      e.active = false;
+      return e;
+    });
+    newTabTxt[index].active = !that.data.tabTxt[index].active;
+    this.setData({
+      tabTxt: data
+    })
+
+  },
+  filterTabChild: function (e) {
+
+    //我需要切换选中项 修改展示文字 并收回抽屉  
+    var that = this;
+    var index = e.currentTarget.dataset.index;
+    var data = JSON.parse(JSON.stringify(that.data.tabTxt));
+    if (typeof (e.target.dataset.id) == 'undefined' || e.target.dataset.id == '') {
+      data[index].active = !that.data.tabTxt[index].active;
+    }
+    else {
+      data[index].type = e.target.dataset.id;
+      data[index].active = !that.data.tabTxt[index].active;
+      if (e.target.dataset.id == '0') {
+        data[index].text = that.data.tabTxt[index].originalText;
+        //不限删除条件
+        delete that.data.searchParam[index];
+      }
+      else {
+        data[index].text = e.target.dataset.txt;
+        //更改删除条件
+        that.data.searchParam[index] = data[index].text;
+      }
+
+
+    }
+
+    that.setData({
+      tabTxt: data
+    })
+    console.log(that.data.searchParam);
+
+
   }
 })
