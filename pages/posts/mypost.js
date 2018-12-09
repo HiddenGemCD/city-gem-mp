f97ec3d3beddcac0ba88513b1888ee15d1961ffe
Chiwei Huang <cwhuang0523@gmail.com>
2018-12-09 22:19:04 +0800

    add map show loation

--- pages/posts/mypost.js
+++ pages/posts/mypost.js
@@ -4,9 +4,28 @@ const myRequest = require('../../lib/api/request');
 Page({
   data: {
     posts: [],
-    flip: {}
+    flip: {},
+    markers: [{
+      iconPath: "/commons/assets/icons/map-marker.png",
+      id: 0,
+      latitude: 31.236125946,
+      longitude: 121.480010986,
+      width: 30,
+      height: 30
+    },
+    // {
+    //   iconPath: "/commons/assets/icons/map-marker.png",
+    //   id: 1,
+    //   latitude: 30.65618,
+    //   longitude: 104.08329,
+    //   width: 30,
+    //   height: 30
+    // }
+    ],
   },
   onLoad: function (options) {
+
+    console.log(this.data.markers)
     // console.log(app.globalData.userId.id)
     let page = this
     let user_id = app.globalData.userId.id
@@ -16,6 +35,10 @@ Page({
         page.setData({ posts: res.data.posts })
       }
     })
+  },
+  onReady: function (e) {
+    // 使用 wx.createMapContext 获取 map 上下文
+    this.mapCtx = wx.createMapContext('myMap')
   },
   newPost: function () {
     wx.navigateTo({
@@ -41,22 +64,36 @@ Page({
       }
     })
   },
+  includePoints: function () {
+
+  },
   flip: function (e) {
-    // this.setData({
-    //   _num: e.currentTarget.id
-    // })
-    
     let that = this
+    let posts = that.data.posts
     let id = e.currentTarget.id
     let flip = that.data.flip
-    // console.log(flip[id])
-    // console.log(flip[e.currentTarget.id])
-    flip[id] = !flip[id]
-    console.log(flip[id])
 
-    this.setData({
-      flip: that.data.flip
+    let markers = that.data.markers
+    markers.latitude = posts[id].latitude
+    markers.longitude = posts[id].longitude
+
+
+
+    posts.forEach(function (item, index) {
+      flip[index] = false
     })
 
+    flip[id] = !flip[id]
+
+    this.setData({
+      markers: that.data.markers,
+    })
+
+    this.setData({
+      flip: that.data.flip,
+    })
+    console.log(markers)
+    console.log(this.data)
+    
   }
 })
--- pages/test/test.js
+++ pages/test/test.js
@@ -1,52 +1,13 @@
-const app = getApp();
-const myRequest = require('../../lib/api/request');
-
+// map.js
 Page({
   data: {
-    posts: []
-  },
-  onLoad: function (options) {
-    // console.log(app.globalData.userId.avatar)
-    let page = this
-    let user_id = app.globalData.userId.id
-    page.setData( {user: app.globalData.userId})
-    myRequest.get({
-      path: 'users/' + user_id + '/posts/by_recent',
-      success(res) {
-        page.setData({ posts: res.data.posts })
-      }
-    })
-  },
-  newPost: function () {
-    wx.navigateTo({
-      url: '/pages/posts/new'
-    })
-  },
-  editPost: function (e) {
-    const post_id = e.target.id;
-    wx.navigateTo({
-      url: '/pages/posts/edit?id=' + post_id
-    })
-  },
-  deletePost: function (e) {
-    console.log(e.target)
-    const post_id = e.target.id;
-    console.log(post_id)
-    myRequest.delete({
-      path: 'posts/' + post_id,
-      success(res) {
-        wx.reLaunch({
-          url: '/pages/posts/mypost'
-        })
-      }
-    })
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    markers: [{
+      iconPath: "/commons/assets/icons/map-marker.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
   }
 })
